fed95409ebf56645ac8f21e72258531740e66e31
victorsklyanik <31613921+victorsklyanik@users.noreply.github.com>
2018-08-26 15:11:22 +0300

    Add files via upload

---
+++ init.js
@@ -0,0 +1,11 @@
+var config = {
+  '.chosen-select'           : { disable_search: true },
+  '.chosen-select-deselect'  : { allow_single_deselect: true },
+  '.chosen-select-no-single' : { disable_search_threshold: 10 },
+  '.chosen-select-no-results': { no_results_text: 'РџСѓСЃС‚Рѕ!' },
+  '.chosen-select-rtl'       : { rtl: true },
+  '.chosen-select-width'     : { width: '95%' }
+}
+for (var selector in config) {
+  $(selector).chosen(config[selector]);
+}
